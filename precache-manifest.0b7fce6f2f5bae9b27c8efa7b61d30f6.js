1624da82355393c1b58d37b6198104d16e3b97a1
J Scott <jbscott@gmail.com>
2019-08-13 15:52:25 -0400

    Updates

---
+++ precache-manifest.0b7fce6f2f5bae9b27c8efa7b61d30f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65c9a3cfb3b3e4a5ad01e44421df7b1",
+    "url": "/flight-training-react/index.html"
+  },
+  {
+    "revision": "0f776aeae1d39bdc8bdc",
+    "url": "/flight-training-react/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "64d918e90d9eb6627e2b",
+    "url": "/flight-training-react/static/css/main.0e970fa0.chunk.css"
+  },
+  {
+    "revision": "0f776aeae1d39bdc8bdc",
+    "url": "/flight-training-react/static/js/2.08f1aa44.chunk.js"
+  },
+  {
+    "revision": "64d918e90d9eb6627e2b",
+    "url": "/flight-training-react/static/js/main.15e5c5e8.chunk.js"
+  },
+  {
+    "revision": "31a1c14afd5af874a2dd",
+    "url": "/flight-training-react/static/js/runtime~main.4352b36f.js"
+  }
+]);
--- src/images.js
+++
@@ -1,7 +0,0 @@
-export const images = [
-  { imageid: 1, src: './assets/icons8-air-traffic-control-tower-80.png', title: 'tower', description: 'air traffic control tower' },
-
-  { imageid: 2, src: './assets/icons8-airplane-80.png', title: 'airplane', description: 'airplane' },
-
-  { imageid: 3, src: './assets/icons8-drone-80.png', title: 'drone', description: 'drone' }
-]; 
